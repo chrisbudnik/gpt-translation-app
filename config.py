c65202e8bf4042dd06941ee1a32c35ed0cdf3d2f
chrisb <chrism.budnik@gmail.com>
2023-11-14 23:27:41 +0100

    update: rm google todo, add doc strings

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 import streamlit as st
 
+
 class Config:
+    """Configuration class for the marketing translator app."""
 
     SERVICE_NAME = "marketing-translator"
     AUTH_SERVICE_URL = "https://my-custom-auth-service.com/grant_access"
@@ -14,14 +16,10 @@ class Config:
 
         if not os.getenv("OPENAI_API_KEY"):
             st.error("Please set your OPENAI_API_KEY environment variable.")
-
-        #TODO: add google vertex ai credentials?
         
         if not os.getenv("GLOBAL_PASSWORD"):
             st.error("Please set your GLOBAL_PASSWORD environment variable.")
 
         if not os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
             st.error("Please set your GOOGLE_APPLICATION_CREDENTIALS environment variable.")
-
-    #TODO: method to check service account permissions to use google vertex ai
         
